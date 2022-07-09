43d6a0193dff0bedd11f71e9765b9dcb88b58bad
Ronald Suplina <tyger995@gmail.com>
2022-07-09 14:57:18 +0100

    Audio Text Mood Analyzer

---
+++ audio.py
@@ -0,0 +1,16 @@
+from speech_recognition import Recognizer, AudioFile
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+recognizer = Recognizer()
+with AudioFile('chile.wav') as audio_file:
+    audio = recognizer.record(audio_file)
+text = recognizer.recognize_google(audio)
+# print(text)
+analyzer = SentimentIntensityAnalyzer()
+scores = analyzer.polarity_scores(text)
+print(scores)
+if scores['compound'] > 0:
+    print('Positive Speech')
+else:
+    print('Negative Speech')
