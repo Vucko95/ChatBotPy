74d367c1c1e59ee36f9088b8075a6d92ffc357cb
Ronald Suplina <tyger995@gmail.com>
2022-07-09 10:23:24 +0100

    Chat Bot

--- lemme.py
+++ lemme.py
@@ -1,27 +1,53 @@
-from nltk.stem import WordNetLemmatizer
 import nltk
-# nltk.download()
-# RUNS ONLY ONCE
-lemmatizer = WordNetLemmatizer()
-sentence = 'Mine grandmas are inside  grocery shop'
-sentence_tokens = nltk.word_tokenize(sentence.lower())
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-pos_tags = nltk.pos_tag(sentence_tokens)
-# print(pos_tags)
-# For sentence we use tokens
+import wikipedia
+
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('wordnet')
+# nltk.download('punkt')
+
+text = wikipedia.page('Vegetables').content
+
+lemmatizer = WordNetLemmatizer()
 
 
 def lemma_me(sent):
     sentence_tokens = nltk.word_tokenize(sent.lower())
+    pos_tags = nltk.pos_tag(sentence_tokens)
+
     sentence_lemmas = []
     for token, pos_tag in zip(sentence_tokens, pos_tags):
         if pos_tag[1][0].lower() in ['n', 'v', 'a', 'r']:
             lemma = lemmatizer.lemmatize(token, pos_tag[1][0].lower())
             sentence_lemmas.append(lemma)
-    print(sentence_lemmas)
+
     return sentence_lemmas
 
 
-sent = input("Please Enter Sentence\n")
-lemma_me(sent)
-# lemma2 = lemmatizer.lemmatize('fruit', 'n')
+def process(text, question):
+    sentence_tokens = nltk.sent_tokenize(text)
+    sentence_tokens.append(question)
+
+    tv = TfidfVectorizer(tokenizer=lemma_me)
+    tf = tv.fit_transform(sentence_tokens)
+    values = cosine_similarity(tf[-1], tf)
+    index = values.argsort()[0][-2]
+    values_flat = values.flatten()
+    values_flat.sort()
+    coeff = values_flat[-2]
+    if coeff > 0.3:
+        return sentence_tokens[index]
+
+
+while True:
+    question = input("Hi, what do you want to know?\n")
+    output = process(text, question)
+    if output:
+        print(output)
+    elif question == 'quit':
+        break
+    else:
+        print("I don't know.")
